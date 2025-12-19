c5466957c9962b32e09875fdd36ac0a589d3fc6a
Bryan Melanson <bryan@bryanmelanson.com>
2025-12-19 10:26:41 -0500

    Update resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -228,7 +228,7 @@ export default {
     // Fetch the JSON Resume data from the file
     try {
       const response = await fetch(
-        "https://gist.githubusercontent.com/bryan-melanson/38b5e5494f4a9776fe6996cebb4cc1fb/raw/c1fbfcdec96da26df2708d21cb6ed3ef160b2601/resume.json",
+        "https://gist.githubusercontent.com/bryan-melanson/38b5e5494f4a9776fe6996cebb4cc1fb/raw/2b3f5a419044cc28d83605cb85c84e7bd81fbda3/resume.json",
       );
       if (response.ok) {
         this.resume = await response.json();
